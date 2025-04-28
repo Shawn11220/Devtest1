be2b071bec1d55196c05956ce69dc4857d6663ec
Shawn11220 <shawnfrost11220@gmail.com>
2025-04-28 23:57:39 +0530

    added test route

--- testing.py
+++ testing.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "Hello, Shawn!"
 
+@app.route('/test')
+def test():
+    return "This is a test route!"
+
 if __name__ == '__main__':
     app.run(debug=True)
